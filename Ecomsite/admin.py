f4388b64da351368248ae5ef27789e9f4809acdf
Shani871 <chauhan145@gmail.com>
2025-01-14 12:25:19 +0530

    Initial commit

---
+++ Ecomsite/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from Ecomsite.models import Product
+
+# Register your models here.
+admin.site.register(Product)
---
+++ Ecomsite/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from Ecomsite.models import Product
+from django.core.paginator import Paginator
+
+
+def index(request):
+    product_object = Product.objects.all()
+
+    # search code
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_object = product_object.filter(title__icontains=item_name)
+
+    # paginator code
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+
+    return render(request, 'shop/index.html', {'product_object': product_object})
