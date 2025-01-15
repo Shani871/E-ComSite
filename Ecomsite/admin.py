a91d2275829e497998868b4e0bbbe0585d16b5ad
Shani871 <chauhan145@gmail.com>
2025-01-15 18:33:34 +0530

    Design Admin panel

--- Ecomsite/admin.py
+++ Ecomsite/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from Ecomsite.models import Product, Order
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.site_header= 'Ecomsite'
+admin.site.index_title = "Mange Ecomsite"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        self.change_category_to_default.short_description = 'Default Category'
+    list_display = ('title','price','discount_price','category','description','image')
+    search_fields = ('title',)
+    fields = ('title',)
+    list_editable = ('price','category',)
+
+
+
+
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Order)
